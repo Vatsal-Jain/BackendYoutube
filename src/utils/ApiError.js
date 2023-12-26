ac75dd75f0601c20b0724425f941aba3582cd8ea
vatsal <vatsal@texlaculture.com>
2023-12-26 12:10:53 +0530

    api response added

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -10,7 +10,7 @@ class ApiError extends Error {
     this.data = null;
     this.message = message;
     this.success = false;
-    this.errors = this.errors;
+    this.errors = errors;
 
     if (stack) {
       this.stack = stack;
@@ -19,3 +19,5 @@ class ApiError extends Error {
     }
   }
 }
+
+export { ApiError };
